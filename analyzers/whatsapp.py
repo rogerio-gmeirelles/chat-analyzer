2a046e56e55e011ae312b0df02a1f701326ab19b
Rogério Meirelles <rogerio.gmeirelles@gmail.com>
2021-05-26 11:03:40 -0300

    implements proccessers

--- analyzers/whatsapp.py
+++ analyzers/whatsapp.py
@@ -1,5 +1,14 @@
-chatData = {}
-
+chatData = {
+    "authors": {}
+}
+startingAuthorData = {
+    "audioCounter": 0,
+    "mediaCounter": 0,
+    "messageCounter": 0,
+    "wordCounter": 0,
+    "goodMorningCounter": 0,
+    "stickerCounter": 0,
+}
 
 def splitLines(chat):
     splitChat = []
@@ -8,8 +17,8 @@ def splitLines(chat):
     # iterate through every character
     for idx, character in enumerate(chat[:-1]):
         if (character == "\n"):
-            # if next line starts with "20", assumes it's a new message
-            if chat[idx + 1] == "2" and chat[idx + 2] == "0":
+            # assumes it's a new message if next line starts with "****/**/**,"
+            if chat[idx + 5] == "/" and chat[idx + 8] == "/" and chat[idx + 11] == ",":
                 splitChat.append(currentLine)
                 currentLine = ""
             else:
@@ -19,23 +28,58 @@ def splitLines(chat):
 
     return splitChat
 
-def proccessAudio(line):
-    # alwats ends with "ptt"
-    # count auduo as 30sec readtime
-    # count audio as 1 msg from person and as 1 audio
-    print(line)
+# gets authour of message and creates initial state if necessary
+def getAuthor(messageData):
+    author = messageData.split(" - ", 1)[1]
+    if not author in chatData["authors"]:
+        chatData["authors"][author] = startingAuthorData
+    return author
 
+def proccessNonTextMessage(line):
+    if "this message has been deleted" in line:
+        return
+    elif "missed video call" in line or "missed voice call" in line:
+        return
+    elif "document" in line:
+        # document attachment
+        return
+    elif "vcard" in line:
+        # contact card
+        return
+    elif "sticker" in line:
+        splitLine = line.split("sticker", 1)
+        author = getAuthor(splitLine[0])
+        chatData["authors"][author]["stickerCounter"] += 1
+        return
+    elif "ptt" in line:
+        # audio file - count as 30sec readtime
+        splitLine = line.split("ptt", 1)
+        author = getAuthor(splitLine[0])
+        chatData["authors"][author]["audioCounter"] += 1
+        return
+    # ignores unforseen scenarios, uncomment the print line if you would like to investigate
+    # "missed group voice call" falls here
+    # print(line)
+    return
+
+# proccess text messages and analyzes behaviour
 def proccessMessage(line):
     splitLine = line.split(": ", 1)
-    autor = splitLine[0].split(" - ", 1)[1]
+    author = getAuthor(splitLine[0])
     message = splitLine[1]
-    print(autor + "|||" + message)
 
+    chatData["authors"][author]["messageCounter"] += 1
+    if "m di" in message or "buenos" in message or "oa ta" in message:
+        chatData["authors"][author]["goodMorningCounter"] += 1
+
+# analyzes the entire whatsapp chat
 def whatsappAnalyzer(chat):
     # read file line by line
     for line in splitLines(chat):
         if ": " in line:
             proccessMessage(line)
         else:
-            proccessAudio(line)
+            proccessNonTextMessage(line)
+
+    # prints resulting analyzed data
     print(chatData)
