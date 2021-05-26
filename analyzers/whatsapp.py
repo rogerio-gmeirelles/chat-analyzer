e966ac2643454acbc31cbb76f42809d3abb8dacc
Rogério Meirelles <rogerio.gmeirelles@gmail.com>
2021-05-26 09:40:11 -0300

    implements line iteration

--- analyzers/whatsapp.py
+++ analyzers/whatsapp.py
@@ -1,2 +1,34 @@
+def splitLines(chat):
+    splitChat = []
+    currentLine = ""
+
+    # iterate through every character
+    for idx, character in enumerate(chat[:-1]):
+        if (character == "\n"):
+            # if next line starts with "20", assumes it's a new message
+            if chat[idx + 1] == "2" and chat[idx + 2] == "0":
+                splitChat.append(currentLine)
+                currentLine = ""
+            else:
+                currentLine += " "
+        else:
+            currentLine += character
+
+    return splitChat
+
+def proccessAudio(line):
+    # alwats ends with "ptt"
+    # count auduo as 30sec readtime
+    # count audio as 1 msg from person and as 1 audio
+    print(line)
+
+def proccessMessage(line):
+    print(line)
+
 def whatsappAnalyzer(chat):
-    print(chat)
+    # read file line by line
+    for line in splitLines(chat):
+        if ":" in line:
+            proccessMessage(line)
+        else:
+            proccessAudio(line)
